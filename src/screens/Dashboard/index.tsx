dcdb2bd7e4843b7dce8f9644ca477529026ed634
Matheus Valandro <msvalandro@Matheuss-MacBook-Pro.local>
2022-05-17 15:12:51 -0300

    save work

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { HighlightCard } from '../../components/HighlightCard';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/21278597?v=4',
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Matheus</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  );
+}
